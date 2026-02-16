87af5b9cb122e84969b3721b00dd7f4ae4cda1a8
yd5769 <yd5769@srmist.edu.in>
2026-02-16 14:00:22 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
---
+++ OOPS.java
@@ -0,0 +1,5 @@
+class OOPS {
+public static void main(String args[]) {
+System.out.println("This is main branch");
+}
+}
